65686e37038892d77f9387832359056d48e82412
ancient225 <91169811+ancient225@users.noreply.github.com>
2024-05-27 12:05:04 +0100

    change to names, scripts and built the project
    changed the name of some things for clarity, and changes to scripts, and then built the project

--- Assets/To Show changing.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ToShowchanging : MonoBehaviour
-{
-    // This script was made to show changes on GitHub as I am not working as a team and dont have any changes to make.
-}
